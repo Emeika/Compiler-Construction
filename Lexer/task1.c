44331d2d1402c153dd5f8007ba351bc04959487b
emeika <shahbazhafsa2014@gmail.com>
2024-05-18 18:10:01 +0500

    Refactor into functions

--- Lexer/task1.c
+++ Lexer/task1.c
@@ -2,25 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
+void tokenize_expression(char *expression)
 {
-    if (argc != 2)
-    {
-        printf("Usage: %s <expression>\n", argv[0]);
-        return 1;
-    }
-
-    char *expression = argv[1];
-    int operand_count = 1;
-
-    if (expression[strlen(expression) - 1] != '$' || strlen(expression) > 10)
-    {
-        printf("Invalid string. must end with $ and maximum 5 operands\n");
-        return 1;
-    }
-
     printf("Program finds following tokens in the expression:\n");
     printf("Expression received: %s\n", expression);
+
+    int operand_count = 0;
 
     while (*expression != '$')
     {
@@ -47,11 +34,30 @@ int main(int argc, char *argv[])
         else
         {
             printf("Invalid token encountered. Program terminated prematurely.\n");
-            return 1;
+            exit(1);
         }
 
         expression++;
     }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: %s <expression>\n", argv[0]);
+        return 1;
+    }
+
+    char *expression = argv[1];
+
+    if (expression[strlen(expression) - 1] != '$' || strlen(expression) > 10)
+    {
+        printf("Invalid string. must end with $ and maximum 5 operands\n");
+        return 1;
+    }
+
+    tokenize_expression(expression);
 
     return 0;
 }
