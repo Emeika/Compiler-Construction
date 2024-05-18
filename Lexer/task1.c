37fb295530008eeb32f500ce3ebc1a22afa73553
emeika <shahbazhafsa2014@gmail.com>
2024-05-18 17:50:57 +0500

    invalid token check

--- Lexer/task1.c
+++ Lexer/task1.c
@@ -11,8 +11,9 @@ int main(int argc, char *argv[])
     }
 
     char *expression = argv[1];
+    int operand_count = 1;
 
-    if (expression[strlen(expression) - 1] != '$' || strlen(expression) > 11)
+    if (expression[strlen(expression) - 1] != '$' || strlen(expression) > 10)
     {
         printf("Invalid string. must end with $ and maximum 5 operands\n");
         return 1;
@@ -23,22 +24,34 @@ int main(int argc, char *argv[])
 
     while (*expression != '$')
     {
-        switch (*expression)
+        if ((*expression >= 'a' && *expression <= 'z') ||
+            *expression == '+' || *expression == '-' ||
+            *expression == '*' || *expression == '/')
         {
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-            printf("Arithmetic operator: %c\n", *expression);
-            break;
-        default:
-            break;
+
+            if (*expression >= 'a' && *expression <= 'z')
+            {
+                operand_count++;
+                printf("int literal found: %c\n", *expression);
+                if (operand_count > 5)
+                {
+                    printf("More than five operands found.\n");
+                    exit(1);
+                }
+            }
+            else
+            {
+                printf("Arithmetic operator: %c\n", *expression);
+            }
         }
-        if (*expression >= 'a' && *expression <= 'z')
+        else
         {
-            printf("int literal found: %c\n", *expression);
+            printf("Invalid token encountered. Program terminated prematurely.\n");
+            return 1;
         }
 
         expression++;
     }
+
+    return 0;
 }
